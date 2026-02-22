e41f6e0364ae62c9e1fa4fdaa181846d106afc83
Tran Dang Khoi <khoi23122005@gmail.com>
2026-02-22 13:09:51 +0700

    initiate new profile card

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+let Card = () => {
+
+    return(
+        <div className="card">
+            <image></image>
+            <h2>Nariyuki</h2>
+            <p>I learn react & react native</p>
+        </div>
+    );
+}
+
+export default Card
