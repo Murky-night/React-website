17fee843137b13ceb2459bd4af3d8cfdbae29f11
Tran Dang Khoi <khoi23122005@gmail.com>
2026-02-23 15:24:32 +0700

    profile card

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,10 +1,11 @@
+import profilePicture from "./assets/168012aec643913546d97a12a4e5bfec.png"
 let Card = () => {
 
     return(
         <div className="card">
-            <image></image>
-            <h2>Nariyuki</h2>
-            <p>I learn react & react native</p>
+            <img className="card-pic"src={profilePicture} alt = "profile picture"></img>
+            <h2 className="card-title">Nariyuki</h2>
+            <p className="card-content">I learn react & react native</p>
         </div>
     );
 }
